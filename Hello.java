43a6e1373a92e4ad9ef0386d8d34a5e9f3e841fd
lilyanniii <111990625+lilyanniii@users.noreply.github.com>
2022-08-24 09:39:07 -0400

    Hello.java in repository

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/********************************************************
+* Hello.java
+*********************************************************
+*Gianni Bautista
+*8/23/2022
+*CMSC 255-004
+********************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println ("Gianni Bautista");
+		System.out.println ("Hello.java");
+		System.out.println ("Lab 1 Assignment");
+		System.out.println ("8/23/2022");
+		System.out.println ("255-004");
+		System.out.println();
+		
+	}
+}
